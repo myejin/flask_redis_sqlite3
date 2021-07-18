05ebb8989ef501cd1bad8bbbbf8f9193440611d9
Ubuntu <ubuntu@ip-172-31-12-28.ap-northeast-2.compute.internal>
2021-07-18 10:56:10 +0000

    리뷰등록API 수정
    바로 sqlite3에 접근하지 않고, redis 큐에 적재시킨다.

--- project/api.py
+++ project/api.py
@@ -1,6 +1,7 @@
 import json
 from flask import jsonify, Blueprint, request 
-from .models import get_posts, datetime_modify, get_specific_reviews, insert_review
+from .models import get_posts, datetime_modify, get_specific_reviews
+from . import rq 
 bp = Blueprint("board", __name__)
 
 def get_response(error, data):
@@ -43,17 +44,16 @@ def reviews_specific_board(boardId):
     req = request.args 
 
     error, data = get_specific_reviews(int(boardId))
-
-    resp = get_response(error, data)    
+    resp = get_response(error, data)     
     return jsonify(resp)
 
 
 @bp.route("/board/<boardId>/review/add", methods = ['POST'])
 def reviews_add(boardId): 
     req = request.get_json()
-
-    error, data = insert_review(boardId, req["userId"], req["point"])
-
-    resp = get_response(error, data)    
+    req['boardId'] = boardId 
+    rq.lpush('review', json.dumps(req))
+    resp = get_response(None, '리뷰 입력 중입니다.')    
     return jsonify(resp)
 # curl -X POST -H 'Accept: application/json' -H 'Content-Type: application/json' "http://localhost:5000/board/1/review/add" -d '{ "userId": "test1", "point": 2 }'
+
