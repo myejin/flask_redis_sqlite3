9d8e5cdadec8fbb615c0c49c63ce8c576538b6b5
Ubuntu <ubuntu@ip-172-31-5-93.ap-northeast-2.compute.internal>
2021-07-19 13:30:55 +0000

    병렬화 가능
    rdb.commit() 후 락킹이 풀리면 다른 프로세스가 DB에 접근하는 것을 확인
    동기방식이라면, time.sleep(2) 기다린 후 다음 요청 처리

--- worker.py
+++ worker.py
@@ -6,37 +6,32 @@ import sqlite3
 import config 
 
 def insert_review(rdb, msg):
-    # 새 리뷰 삽입
-    msg = json.loads(msg)
-    boardId, userId, point = msg["boardId"], msg["userId"], msg["point"]
+    '''
+    새 리뷰 삽입,
+    리뷰 누적평점 및 누적개수 업데이트
+    '''
+    try:
+        msg = json.loads(msg)
+        boardId, userId, point = msg["boardId"], msg["userId"], msg["point"]
     
-    before = rdb.execute('SELECT count(*) FROM reviews').fetchone()[0]
-    rdb.execute('INSERT INTO reviews(id, board_id, user_id, point) VALUES (null, ?, ?, ?)', (boardId, userId, point))
-    after = rdb.execute('SELECT count(*) FROM reviews').fetchone()[0]
-    
-    if after == before:
-        return f'리뷰 등록을 실패했습니다.'
-    return None
+        rdb.execute('INSERT INTO reviews(id, board_id, user_id, point) VALUES (null, ?, ?, ?)', (boardId, userId, point,)) 
+        total_point, total_rev_cnt = rdb.execute('SELECT total_point, total_rev_cnt FROM board WHERE id = ?', (boardId,)).fetchone()
+        rdb.execute('UPDATE board SET total_point = ? WHERE id = ?', (total_point + point, boardId,))
+        rdb.execute('UPDATE board SET total_rev_cnt = ? WHERE id = ?', (total_rev_cnt + 1, boardId,))
+        rdb.commit()
+        
+        time.sleep(2)  # 연산속도 느리게 하기        
+        return None, f'PID [{os.getpid()}] 리뷰 등록 완료' 
+    except sqlite3.DatabaseError as e:
+        return e, None 
 
 
 rq = redis.from_url(config.CONFIG['REDIS_URL'])
+rdb = sqlite3.connect(
+        config.CONFIG['DATABASE']
+)
 while True:
-    if not rq.llen('review'):
-        continue
-
-    rdb = sqlite3.connect(
-        config.CONFIG['DATABASE'],
-        isolation_level=None    
-    )
-    count = min(10, rq.llen('review'))
-    print('쌓인 데이터 수: ', count)
-    i = 0
-    while i < count:
-        msg = rq.rpop('review')
-        ret = insert_review(rdb, msg)
-        if ret is None:
-            print('리뷰 등록 완료') 
-        #else: # 에러가 있다면..
-        time.sleep(2)  # 연산속도 느리게 하기        
-        i += 1
-    rdb.close()
+    msg = rq.brpop('review', 0)[1]
+    error, data = insert_review(rdb, msg)
+    print({'error': error, 'data': data})
+rdb.close()
