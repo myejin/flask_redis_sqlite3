9679459d87a647cc4356c5870cddf4d094c74a55
Ubuntu <ubuntu@ip-172-31-5-93.ap-northeast-2.compute.internal>
2021-07-19 13:09:22 +0000

    함수구조 변경 및 오류수정
    try-except문으로 변경하여 에러처리, get_specific_reviews 오류 수정

--- project/models.py
+++ project/models.py
@@ -3,47 +3,58 @@ import json
 from . import db
 
 def get_posts(cat):
-    # 카테고리에 따른 게시글 조회
-    if cat is None:
-        rows = db.get_db().execute('SELECT *, total_point/total_rev_cnt as aver FROM board ORDER BY aver DESC, id DESC')
-    else:
-        rows = db.get_db().execute('SELECT *, total_point/total_rev_cnt as aver FROM board WHERE category = ? ORDER BY aver DESC, id DESC', (cat,))
-
-    data = []
-    for row in rows:
-        aver = 0
-        if row[8]:
-            aver = row[8]
-        data.append({'id': row[0], 'title': row[1], 'writer': row[2], 'category': row[3], 'post_time': row[4], 'last_modify_at': row[5], 'average_review_point': aver})
-    
-    if not data:
+    '''
+    카테고리에 따른 게시글 조회
+    '''
+    try:
         if cat is None:
-            cat = '전체'
-        return f'카테고리 {cat}의 게시물이 없습니다.', None
-    return None, data 
+            rows = db.get_db().execute('SELECT *, total_point/total_rev_cnt as aver FROM board ORDER BY aver DESC, id DESC')
+        else:
+            rows = db.get_db().execute('SELECT *, total_point/total_rev_cnt as aver FROM board WHERE category = ? ORDER BY aver DESC, id DESC', (cat,))
+
+        data = []
+        for row in rows:
+            aver = 0
+            if row[8]:
+                aver = row[8]
+            data.append({'id': row[0], 'title': row[1], 'writer': row[2], 'category': row[3], 'post_time': row[4], 'last_modify_at': row[5], 'average_review_point': aver})
+    
+        if not data:
+            if cat is None:
+                cat = '전체'
+            data = f'카테고리 {cat}의 게시물이 없습니다.'
+        return None, data 
+    except sqlite3.DatabaseError as e:
+        return e, None 
 
 
 def datetime_modify(id):
-    # 게시글 수정으로 last_modify_at 업데이트 
-    before = db.get_db().execute('SELECT last_modify_at as aver FROM board WHERE id = ?', (id,)).fetchone()[0]
-    db.get_db().execute('UPDATE board SET last_modify_at = CURRENT_TIMESTAMP WHERE id = ?', (id, ))
-    after = db.get_db().execute('SELECT last_modify_at FROM board WHERE id = ?', (id,)).fetchone()[0]
-
-    if after == before:
-        return f'게시글 수정을 실패했습니다.', None
-
-    data = [{'before': before, 'after': after}]
-    return None, data
+    '''
+    게시글 수정으로 last_modify_at 업데이트 
+    '''
+    try:
+        db.get_db().execute('UPDATE board SET last_modify_at = CURRENT_TIMESTAMP WHERE id = ?', (id, ))
+        db.get_db().commit()
+        row = db.get_db().execute('SELECT last_modify_at FROM board WHERE id = ?', (id,)).fetchone()[0]
+        data = [{ 'last_modify_at' : row }]
+        return None, data
+    except sqlite3.DatabaseError as e:
+        return e, None 
 
 
 def get_specific_reviews(boardId):
-    # 특정 게시글의 리뷰 조회
-    rows = db.get_db().execute('SELECT * FROM reviews')
-    data = []
-    for row in rows:
-        data.append({"id": row[0], "board_id": row[1], "user_id": row[2], "point": row[3]})
-    print(data)
+    '''
+    특정 게시글의 리뷰 조회
+    '''
+    try:
+        rows = db.get_db().execute('SELECT * FROM reviews WHERE board_id = ?', (boardId,))
+        data = []
+        for row in rows:
+            data.append({'id': row[0], 'board_id': row[1], 'user_id': row[2], 'point': row[3]})
 
-    if not data:
-        return f'게시글 {boardId}의 리뷰가 없습니다.', None
-    return None, data 
+        if not data:
+            data = f'게시글 {boardId}의 리뷰가 없습니다.'
+        return None, data 
+    except sqlite3.DatabaseError as e:
+        return e, None 
+    
