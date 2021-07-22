4e8779dce41f305fbe3e7f5542452e6553a4e6d9
Ubuntu <ubuntu@ip-172-31-3-117.ap-northeast-2.compute.internal>
2021-07-22 15:15:51 +0000

    rq 삭제
    글로벌로 선언했던 rq를 삭제함, 대신 db.py 에 get_redis 함수를 생성하였다.

--- project/__init__.py
+++ project/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask
-import redis 
 
-rq = redis.Redis()
 
 def create_app(config_file=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_file, silent=True)
     from . import db
     db.init_app(app)
-
-    rq = redis.from_url(app.config['REDIS_URL'])
 
     from . import api 
     app.register_blueprint(api.bp)
